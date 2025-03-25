c5aa1ec94b772919f0d3e81540730bc235d5ff54
Carlos Henrique Rodrigues Ribeiro <139514571+carloshenrique2701@users.noreply.github.com>
2025-03-25 20:38:54 -0300

    Update Database.sql

--- banco/Database.sql
+++ banco/Database.sql
@@ -3,14 +3,19 @@ create database Jogo_Dados;
 use Jogo_Dados;
 
 create table Usuario(
-	Id int primary key not null,
-	nome varchar(100) not null,
-    idade date not null,
-    email varchar(100)not null,
-    senha varchar(20) not null
+    id_usuario int primary key not null auto_increment,
+    nome varchar(100) not null,
+    idade int not null,
+    email varchar(100) not null,
+    senha varchar(20) not null,
+    id_jogador int,  
+    foreign key (id_jogador) references Jogador(id_jogador)  
 );
 
 create table Jogador(
-	nome varchar(20) not null,
-    record time 
+    id_jogador int primary key not null auto_increment,
+    nome varchar(100) not null, 
+    record time,  
+    id_usuario int,  
+    foreign key (id_usuario) references Usuario(id_usuario) 
 );
