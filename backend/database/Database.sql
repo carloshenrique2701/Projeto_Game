a630009291f8547bd25c22b3e9c0e06378c25156
Carlos <carloshrr1209@gmail.com>
2025-04-26 21:27:29 -0300

    Alteração no BD

--- backend/database/Database.sql
+++ backend/database/Database.sql
@@ -1,6 +1,8 @@
+/*testes
+
 create database Jogo_Dados;
 
-/* Convertido para postgresql */;
+
 
 -- SQLINES FOR EVALUATION USE ONLY (14 DAYS)
 create table Usuario(
@@ -19,3 +21,28 @@ create table Jogador(
     id_usuario int,  
     foreign key (id_usuario) references Usuario(id_usuario) 
 );
+
+*/
+
+
+create database Jogo_Dados;
+
+use Jogo_Dados;
+
+create table Usuario(
+    id_usuario int primary key not null auto_increment,
+    nome varchar(100) not null,
+    email varchar(100) not null,
+    senha varchar(20) not null,
+    id_jogador int,  
+    foreign key (id_jogador) references Jogador(id_jogador)  
+);
+
+create table Jogador(
+    id_jogador int primary key not null auto_increment,
+    nome varchar(100) not null, 
+    record time,  
+    id_usuario int,  
+    foreign key (id_usuario) references Usuario(id_usuario) 
+);
+
