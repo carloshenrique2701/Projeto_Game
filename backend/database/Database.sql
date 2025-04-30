dfb6bda3bf25a967b094d451d4236115ddb988cf
Carlos <carloshrr1209@gmail.com>
2025-04-30 15:38:33 -0300

    Atualizações gerais do front-end

--- backend/database/Database.sql
+++ backend/database/Database.sql
@@ -1,30 +1,3 @@
-/*testes
-
-create database Jogo_Dados;
-
-
-
--- SQLINES FOR EVALUATION USE ONLY (14 DAYS)
-create table Usuario(
-    id_usuario int primary key not null generated always as identity,
-    nome varchar(100) not null,
-    email varchar(100) not null,
-    senha varchar(20) not null,
-    id_jogador int,  
-    foreign key (id_jogador) references Jogador(id_jogador)  
-);
-
-create table Jogador(
-    id_jogador int primary key not null generated always as identity,
-    nome varchar(100) not null, 
-    record time(0),  
-    id_usuario int,  
-    foreign key (id_usuario) references Usuario(id_usuario) 
-);
-
-*/
-
-
 create database Jogo_Dados;
 
 use Jogo_Dados;
